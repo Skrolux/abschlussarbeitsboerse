171eab3fa3a2cfce0749ff05694d3a6853bab15f
Skrolux <46719449+Skrolux@users.noreply.github.com>
2021-03-25 15:13:33 +0100

    Adjust db.js
    Changed DB_URI to connect to Mongo DB Cloud Database

--- node-backend/src/config/db.js
+++ node-backend/src/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 // bei direkten Verbidungen zwischen Docker-Containern immer Containername als Hostname verwenden
-const DB_URI = "mongodb://mongo:27017/main_database";
+// const DB_URI = "mongodb://mongo:27017/main_database";
+
+const DB_URI = "mongodb+srv://evaluation:5YT32iA74bgH6zn@cluster0.7sfmj.mongodb.net/main_database?retryWrites=true&w=majority"
 
 const connectDB = async () => {
   try {
